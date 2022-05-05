a19ba949eb02cc286f94bd215d8f52bb271563c2
codingchild <codingchild@gmail.com>
2022-05-05 12:00:28 +0000

    model annotation completed

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-todo_list
+# Introduce
+This repository was made for estimating cold start problems in KT models.
+DKT, DKVMN, SAKT, SAINT, GKT are targets for research.
+
+# todo_list
 1. add ASSISTment 2009-2010 datasets
 2. Check the datasets whether Baker's team used or not
+3. Visualizer have to be changed
+4. made SAKT, SAINT, GKT
+
+# Reference
+hcnoh's github
+...
--- src/models/dkvmn.py
+++ src/models/dkvmn.py
@@ -51,48 +51,96 @@ class DKVMN(Module):
         self.a_layer = Linear(self.dim_s, self.dim_s)
 
     def forward(self, q, r):
-        
+
         # 문항과 문제 번호에 따라서 값을 줌
+        #self.num_q는 문항의 갯수
+        #q는 실제 문항의 번호가 담긴 vectors
+        #r은 정오답값
+        #|x| = (bs, 문항의 정오답에 따른 인덱스값) ex) (64, 250)
         x = q + self.num_q * r
 
-        batch_size = x.shape[0]
+        #|batch_size| = (64,)
+        batch_size = x.shape[0] 
+
+        #|Mv0| = (size_m, dim_s), if dim_s and size_m are default, |Mv0| = (20, 50)
+        #|Mvt| = (bs, size_m, dim_s), if dim_s and size_m are default, |Mvt| = (bs, 20, 50)
         Mvt = self.Mv0.unsqueeze(0).repeat(batch_size, 1, 1)
 
+        #Mv는 Mvt를 list로 감싸는 코드, 이유는 아래에서 확인해보기
         Mv = [Mvt]
 
+        #k(key)는 q(question)만을 활용해서 embedding vector를 만들게 됨
+        #|k| = (bs, 문항 인덱스 값, dim_s), ex) (bs, 250, 50)
         k = self.k_emb_layer(q)
+
+        #|v| = (bs, 문항의 정오답에 따른 인덱스값, dim_s)
         v = self.v_emb_layer(x)
 
+        #|Mk| = (size_m, dim_s)
+
+        #|w| = (bs, 문항 인덱스 값, size_m)
         w = torch.softmax(
             torch.matmul(k, self.Mk.T),
             dim = -1
         )
 
         #Write Process
-        e = torch.sigmoid( self.e_layer(v) )
-        a = torch.tanh( self.a_layer(v) )
+
+        #|e| = (bs, 문항의 정오답에 따른 인덱스값, dim_s)
+        e = torch.sigmoid( 
+            self.e_layer(v) #|self.e_layer| = (bs, 문항의 정오답에 따른 인덱스값, dim_s)
+        )
+        #|a| = (bs, 문항의 정오답에 따른 인덱스값, dim_s)
+        a = torch.tanh(
+            self.a_layer(v) #|self.a_layer| = (bs, 문항의 정오답에 따른 인덱스값, dim_s)
+        )
 
         for et, at, wt in zip(
-            e.permute(1, 0, 2), a.permute(1, 0, 2), w.permute(1, 0, 2)
+            e.permute(1, 0, 2), #|e.permute| = (문항의 정오답에 따른 인덱스값, bs, dim_s)
+            a.permute(1, 0, 2), #|a.permute| = (문항의 정오답에 따른 인덱스값, bs, dim_s)
+            w.permute(1, 0, 2)  #|w.permute| = (문항 인덱스 값, bs, size_m)
         ):
+            #|et| = (bs, dim_s)
+            #|at| = (bs, dim_s)
+            #|wt| = (bs, size_m)
+
+            #형상이 맞지 않는 값들을 더하기 위해 unsqueeze를 활용함
+            #|wt.unsqueeze(-1)| = (bs, size_m, 1)
+            #|et.unsqueeze(1)| = (bs, 1, dim_s)
+            #|at.unsqueeze(1)| = (bs, 1, dim_s)
+            #|Mvt| = (bs, size_m, dim_s)
             Mvt = Mvt * (1 - (wt.unsqueeze(-1) * et.unsqueeze(1)) ) + ( wt.unsqueeze(-1) * at.unsqueeze(1) )
+            #Mv는 Mvt들을 보관하는 리스트
             Mv.append(Mvt)
 
-        Mv = torch.stack(Mv, dim = 1)
+        #Mv는 Mvt들이 쌓여있는 리스트, |Mvt| = (bs, size_m, dim_s)
 
+        Mv = torch.stack(Mv, dim = 1)
+        #|Mv| = (bs, Mvt의 갯수, size_m, dim_s)
+
+        #|w.unsqueeze(-1)| = (bs, 문항 인덱스 값, size_m, 1)
+        #|Mv[:, :-1]| = (bs, Mvt의 갯수, size_m, dim_s)
+        #|(w.unsqueeze(-1) * Mv[:, :-1])| = (bs, 문항 인덱스 값, size_m, dim_s)
+        
         #Read Process
         f = torch.tanh(
-            self.f_layer(
+            self.f_layer( 
                 torch.cat(
                     [
+                        #rt임, |(w.unsqueeze(-1) * Mv[:, :-1]).sum(-2)| = (bs, 문항 인덱스 값, dim_s)
                         (w.unsqueeze(-1) * Mv[:, :-1]).sum(-2),
+                        #|k| = (bs, 문항 인덱스 값, dim_s)
                         k
                     ],
-                    dim = -1
+                    dim = -1 #마지막 차원을 기준으로 concat
                 )
             )
         )
 
+        #|f| = (bs, 문항 인덱스 값, dim_s)
+
         p = torch.sigmoid( self.p_layer(f) ).squeeze()
+
+        #|p| = (bs, 문항 인덱스별 확률값)
 
         return p, Mv
